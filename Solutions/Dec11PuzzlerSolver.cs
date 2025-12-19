065d532770a032007561faa1c4feddf824abb699
Carl Hirschman <carlhi@microsoft.com>
2025-12-19 12:17:52 -0800

    Solve Dec 11 Part 2.

--- Solutions/Dec11PuzzlerSolver.cs
+++ Solutions/Dec11PuzzlerSolver.cs
@@ -1,4 +1,5 @@
-﻿using Adventofcode2025.Utilities;
+﻿using System.Security;
+using Adventofcode2025.Utilities;
 
 namespace AdventOfCode2025.Solutions
 {
@@ -6,7 +7,6 @@ namespace AdventOfCode2025.Solutions
     {
         public string SolvePartOne(bool test)
         {
-            // aaa: you hhh
             var devices = new Dictionary<string, List<string>>();
             foreach (string line in PuzzleReader.GetPuzzleInput(11, test))
             {
@@ -47,12 +47,70 @@ namespace AdventOfCode2025.Solutions
 
         public string SolvePartTwo(bool test)
         {
-            throw new NotImplementedException();
+            var devices = new Dictionary<string, List<string>>();
+            foreach (string line in PuzzleReader.GetPuzzleInput(11, test, partTwoTestDifferent: true))
+            {
+                string[] devParts = line.Split(':');
+                string label = devParts[0];
+                devices[label] = new List<string>();
+
+                foreach (string output in devParts[1].Trim().Split(' '))
+                {
+                    devices[label].Add(output);
+                }
+            }
+
+            // Let N(s,t,l) = number of paths from s to t that do not include any devices in l.
+            // N(s, s, *) = 1
+            // N(s, t, l) = sum(o in outputs(s), N(o, t, l))
+
+            var memozied = new Dictionary<(string, string, string[]), long>();
+
+            long dac_fft = NumPaths(devices, "dac", "fft", memozied, "svr");
+
+            long fft_dac = NumPaths(devices, "fft", "dac", memozied, "svr");
+            
+
+            long svr_fft = NumPaths(devices, "svr", "fft", memozied, "dac");
+            long dac_out = NumPaths(devices, "dac", "out", memozied, "svr", "fft");
+
+            long svr_dac = NumPaths(devices, "svr", "dac", memozied, "fft");
+            long fft_out = NumPaths(devices, "fft", "out", memozied, "svr", "dac");
+
+            long numPaths = svr_dac * dac_fft * fft_out + svr_fft * fft_dac * dac_out;
+            return numPaths.ToString();
         }
 
-        private class Device
+        private static long NumPaths(
+            Dictionary<string, List<string>> devices, 
+            string start, 
+            string end,
+            Dictionary<(string, string, string[]), long> memozied,
+            params string[] except)
         {
+            if (start == "out")
+            {
+                return (start == end) ? 1 : 0;
+            }
 
+            if (start == end)
+            {
+                return 1;
+            }
+
+            if (memozied.ContainsKey((start, end, except)))
+            {
+                return memozied[(start, end, except)];
+            }
+
+            long sum = 0;
+            foreach (string output in devices[start])
+            {
+                sum += NumPaths(devices, output, end, memozied, except);
+            }
+
+            memozied[(start, end, except)] = sum;
+            return sum;
         }
     }
 }
--- Utilities/PuzzleReader.cs
+++ Utilities/PuzzleReader.cs
@@ -2,10 +2,11 @@
 {
     internal static class PuzzleReader
     {
-        public static IEnumerable<string> GetPuzzleInput(int day, bool test)
+        public static IEnumerable<string> GetPuzzleInput(int day, bool test, bool partTwoTestDifferent = false)
         {
             string testString = test ? "test" : string.Empty;
-            string filePath = $"c:\\AdventOfCode\\2025\\Dec{day}{testString}.txt";
+            string part2TestSuffix = (test && partTwoTestDifferent) ? "2" : string.Empty;
+            string filePath = $"c:\\AdventOfCode\\2025\\Dec{day}{testString}{part2TestSuffix}.txt";
             using (var reader = new StreamReader(filePath))
             {
                 while (!reader.EndOfStream)
